f73db97d2d82564bb3bebdb9f93fa477c6ddd4c3
Yosuke Furukawa <yosuke.furukawa@gmail.com>
2025-05-26 17:18:55 +0900

    Replace Mocha with Vitest

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+module.exports = [
+  {
+    files: ["**/*.js"],
+    ignores: ["node_modules/**", "eslint.config.js"],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "commonjs",
+    },
+    rules: {}
+  }
+];
